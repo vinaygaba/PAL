167221a0b0ed950f9463ecf6af3371361c285040
Vinay Gaba <vinaygaba@gmail.com>
2016-02-21 17:36:12 -0500

    Added method to merge two pdf's

--- java/PAL-Java/src/com/pal/text/CreatePDFFromExistingPDF.java
+++ java/PAL-Java/src/com/pal/text/CreatePDFFromExistingPDF.java
@@ -2,8 +2,10 @@ package com.pal.text;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 
 public class CreatePDFFromExistingPDF {
 	
@@ -20,6 +22,34 @@ public class CreatePDFFromExistingPDF {
 		// finally make sure that the document is properly
 		// closed.
 		document.close();
+	}
+	
+	/**
+	 * Method to merge multiple PDF's
+	 * @param files List of files that need to be merged
+	 * @param filepath The file path where the output pdf should be saved to
+	 * @throws IOException
+	 */
+	public static void mergePDFs(List<File> files,String filepath)throws IOException{
+		
+		PDDocument doc = new PDDocument();
+		
+		for(File file:files){
+			PDDocument document1 = PDDocument.load(file);
+			
+			for(PDPage pg:document1.getPages()){
+				doc.addPage(pg);
+			}
+			
+		}
+		
+		// Save the newly created document
+		doc.save(filepath);
+
+		// finally make sure that the document is properly
+		// closed.
+		doc.close();
+		
 	}
 
 }
