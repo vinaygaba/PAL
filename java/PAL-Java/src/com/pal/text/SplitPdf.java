fea1ecb73d6b7f843f9beaa4d4f1b2f1150c4d4e
Anshuman Singh <anshuman@Anshumans-MacBook-Pro.local>
2016-02-21 17:39:47 -0500

    Split a pdf into documents and saving documents as different pdfswith ability to set split size

--- java/PAL-Java/src/com/pal/text/SplitPdf.java
+++ java/PAL-Java/src/com/pal/text/SplitPdf.java
@@ -7,6 +7,7 @@ import org.apache.pdfbox.multipdf.Splitter;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class SplitPdf{
+	//Splits document into 1 page sized Pdfs
 	public static void splitPdf(File file) throws IOException{
 				// Load an existing PDF document
 		PDDocument documentToSplit = PDDocument.load(new File("test.pdf"));
@@ -21,4 +22,21 @@ public class SplitPdf{
 			i++;
 		}
 	}
+	//Splits document into 'split' sized Pdfs
+	public static void splitPdfwithSize(File file, int split) throws IOException{
+		// Load an existing PDF document
+		PDDocument documentToSplit = PDDocument.load(new File("test.pdf"));
+		Splitter documentSplitter = new Splitter();
+		documentSplitter.setSplitAtPage(split);
+		// Splitting based on resulting document size (default is 1)
+		List<PDDocument> splitDocuments = documentSplitter.split(documentToSplit);
+		int i = 1;
+		// Saving the split documents as a Pdf
+		for(PDDocument doc : splitDocuments){
+			doc.save("test" + i + ".pdf");
+			doc.close();
+			i++;
+		}
+	}
+	
 }
