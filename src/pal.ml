9ebea5d998624e56c72557022f2754614eeaf51b
Diksha Vanvari <dikshavanvari@Dikshas-MacBook-Pro.local>
2016-04-03 07:31:23 -0400

    Added code to run analyzer and codegen

--- src/pal.ml
+++ src/pal.ml
@@ -1,18 +1,13 @@
-open Ast
-let rec eval = function
-LitString(x) -> x
-| Binop(e1, op, e2) ->
-let v1 = eval e1 and v2 = eval e2 in
-(match op with
-| Add -> ( match v1 with
-| int ->  string_of_int ((int_of_string v1) + (int_of_string v2))
-| string -> v1 ^ v2
-)
-| _ -> v1 ^ v2
-)
+type action = Ast | Sast | Java | Debug
 
 let _ =
-let lexbuf = Lexing.from_channel stdin in
-let expr = Parser.program Lexer.token lexbuf in
-let result = eval expr in
-print_endline (result);;
+  let action = if Array.length Sys.argv > 1 then
+    List.assoc Sys.argv.(1) [ ("-j", Java);]
+  else Java in
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program Scanner.token lexbuf in
+  match action with
+  Java ->
+      let ap = Analyzer.infer_prog program in
+      let _ = Javagen.generateJavaProgram "output" ap in
+      print_string "Success! Compiled to java/output.java\n"
