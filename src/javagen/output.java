4b1f430443185576d27823d4bec220b1a092360f
Diksha Vanvari <dikshavanvari@Dikshas-MacBook-Pro.local>
2016-04-03 20:03:12 -0400

    Fixed a big in the parser

---
+++ src/javagen/output.java
@@ -0,0 +1,11 @@
+
+  public class output
+  {
+      public static void main(String[] args)
+      {
+        PrimitiveObject pageVar1 = new PrimitiveObject(new PDPage());
+PrimitiveObject pageVar2 = new PrimitiveObject(new PDPage());
+pageVar1 = (PrimitiveObject)(PrimtiveObject pageVar2);
+
+      }  
+  }
