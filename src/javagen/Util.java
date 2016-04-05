67d6cf9347cd04939829d20472ff4a6999efe36f
Vinay Gaba <vinaygaba@gmail.com>
2016-04-05 15:30:52 -0400

    Added addLineToTuple method in Util.java

--- src/javagen/Util.java
+++ src/javagen/Util.java
@@ -2,6 +2,9 @@
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class Util{
 
@@ -11,6 +14,25 @@ public class Util{
     doc.addPage(page);
 
     return doc;
+  }
+
+  public static Tuple addLineToTuple(Tuple tuple,Line line){
+    // Start a new content stream which will "hold" the to be created content
+    PDPageContentStream contentStream = new PDPageContentStream(tuple.getDocument(), tuple.getPage());
+
+    // Define a text content stream using the selected font, moving the cursor and drawing the text "Hello World"
+   contentStream.beginText();
+   contentStream.setFont(PDType1Font.TIMES_ROMAN, line.getFontSize());
+   contentStream.moveTextPositionByAmount( line.getXcod(), line.getYcod() );
+   contentStream.drawString(line.getText());
+   contentStream.endText();
+   contentStream.close();
+
+    // Make sure that the content stream is closed:
+    contentStream.close();
+
+
+    return tuple;
   }
 
 
