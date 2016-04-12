efda9abc56315281834db4c0497027118faefc5e
Diksha Vanvari <dikshavanvari@Dikshas-MacBook-Pro.local>
2016-04-12 17:00:15 -0400

    Added codegen logic for for, while, if-else

--- src/codegen.ml
+++ src/codegen.ml
@@ -175,6 +175,20 @@ let location = StringMap.find "2" funcExprMap in
 sprintf "\n%s.save(%s);\n %s.close();" pdfIden location pdfIden
 | _ -> failwith "undefined function"
 
+
+and writeInitAssignStmt iden t expression = 
+let expressionString = generateExpression expression in
+let name = 
+( match iden with
+| IdTest(n ) -> n ) in
+match t with
+| Int -> sprintf "\nInteger %s = %s;" name expressionString
+| String -> sprintf "\nString %s = %s;" name expressionString
+| Bool -> sprintf "\nBoolean %s = %s;" name expressionString
+| Float -> sprintf "\nFloat %s = %s;" name expressionString
+| _ -> failwith "initialization not possible for this type"
+
+
 (*and writeFunctionCallStmt name exprList =
 match name with
 | "renderpdf" ->
@@ -223,10 +237,55 @@ let rec writeDeclarationStmt tid tdataType =
      | TAssign(tid, tExpression ) ->  writeAssignmentStmt tid tExpression
      | TObjectCreate(tid, tspDataType, tExprList ) -> writeObjectStmt tid tspDataType tExprList
      | TCallStmt(name, exprList ) -> writeFunctionCallStmt name exprList
+     | TInitAssign(iden, t, expression) -> writeInitAssignStmt iden t expression
+     | TFor(initStmt, condition, incrStmt, body) -> writeForLoopStatement initStmt condition incrStmt body
+     | TWhile(condition, body) -> writeWhileStatement condition body
+     | TIf(conditionStmt, elsestmtList) -> writeIfBlock conditionStmt elsestmtList
+
 
 and writeStmtList stmtList =
 let outStr = List.fold_left (fun a b -> a ^ (generateStatement b)) "" stmtList in
 sprintf "%s" outStr
+
+
+
+and generateConditionalList conditionList =
+let i = 0 in
+  let concatenatedConditionals = List.fold_left (fun a b -> a ^ (generateConditionStmt b i+1)) "" conditionList in
+    sprintf "%s" concatenatedConditionals
+
+and writeElseStmt body =
+let bodyString = writeStmtList body in
+sprintf " else \n{   %s \n}" bodyString
+
+
+and writeIfBlock conditionList elseBody =
+let conditionListString = generateConditionalList conditionList in
+let elseBodyString = writeElseStmt body in
+sprintf " %s \n %s " conditionListString elseBodyString
+
+and writeForLoopStatement initStmt condition incrStmt body =
+let exprString = generateExpression condition in
+let initStmtString = generateStatement initStmt in
+let incrStmtString = generateStatement incrStmt in
+let incrStmtSubString = String.sub incrStmtString 0 ((String.length incrStmtString) - 2) in
+let bodyString = writeStmtList body in
+sprintf "\nfor(%s %s ; %s) \n { %s \n }" initStmtString exprString incrStmtSubString bodyString
+
+
+and generateConditionStmt conditional index = 
+let ifExpression = generateExpression conditional.tcondition in
+let body = writeStmtList conditional.tbody in
+match index with
+| 1  -> sprintf "\n if (%s)  \n{ \n %s \n}" ifExpression body 
+| _ ->  sprintf "\n else if (%s)  \n{ \n %s \n}" ifExpression body
+
+
+
+and writeWhileStatement condition body = 
+let exprString = generateExpression condition in
+let bodyString = writeStmtList body in
+sprintf "\nwhile(%s ) \n { %s \n }" exprString bodyString
 
 
 and generateJavaProgram fileName prog =
--- src/javagen/output.java
+++ src/javagen/output.java
@@ -1,11 +1,19 @@
 
-  public class output
-  {
-      public static void main(String[] args)
-      {
-        PrimitiveObject pageVar1 = new PrimitiveObject(new PDPage());
-PrimitiveObject pageVar2 = new PrimitiveObject(new PDPage());
-pageVar1 = (PrimitiveObject)(PrimtiveObject pageVar2);
+  import java.io.File;
+  import org.apache.pdfbox.pdmodel.PDDocument;
+  import org.apache.pdfbox.pdmodel.PDPage;
+  import org.apache.pdfbox.pdmodel.PageLayout;
+  import org.apache.pdfbox.pdmodel.font.PDFont;
+  import org.apache.pdfbox.pdmodel.PDPageContentStream;
+  import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
+  public class Output
+  {
+      public static void main(String[] args) throws Exception
+      {
+        
+while(new Integer(5) < new Integer(3) ) 
+ {  
+ }
       }  
   }
