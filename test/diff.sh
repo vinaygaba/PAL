d7c2f22d7b7bf7fb08176d15a616aa6aad9cf368
Vinay Gaba <vinaygaba@gmail.com>
2016-04-16 23:49:19 -0400

    Made changed to diff.sh to check whether a pdf matches the golden copy

--- test/diff.sh
+++ test/diff.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 OUTPUT=output/*
 GOLDEN=golden/*
+DIFFERENCE=differences/*
 OUTPUTIMAGES1=output/images/
 GOLDENIMAGES1=golden/images/
 
 OUTPUTIMAGES2=output/images/*
 GOLDENIMAGES2=golden/images/
+
+DIFFERENCEIMAGES=differences/
 
 pdftk output.pdf burst output output/output_0%d.pdf
 
@@ -32,16 +35,37 @@ do
   fi
 done
 
-sleep 7;
-
 for o in $OUTPUTIMAGES2
 do
     if [ ${o##*.} == png ]
       then
     #echo ${o##*/}
-    compare ${o} $GOLDENIMAGES1${o##*/} -compose Src differences.png
+    compare ${o} $GOLDENIMAGES1${o##*/} -compose Src $DIFFERENCEIMAGES/${o##*/}
     fi
 done
+
+flag=1
+for d in $DIFFERENCE
+do
+  colors=$(identify -format "%k" $d)
+  if [ $colors -ne 1 ]
+  then
+    flag=0
+    #PID=$!
+    #sleep 2
+    #kill -s $PID
+  fi
+done
+
+echo
+echo
+echo "***************** AUTOMATED PDF COMPARISON *****************"
+if [ $flag -eq 1 ]
+then
+  echo 'Hooray! The pdf generated matches the golden copy'
+else
+  echo 'The pdf generated does not match the golden copy'
+fi
 #compare $OUTPUTIMAGES/sunshine1.png $GOLDENIMAGES/sunshine2.png -compose Src differences.png
 
   #pdftk golden.pdf burst output golden/golden_03%d.pdf
